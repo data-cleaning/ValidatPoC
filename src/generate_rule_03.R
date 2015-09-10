68d06519c9e7a85375dc89e8d141c25ac90df3ea
mark <mark.vanderloo@gmail.com>
2015-09-10 13:54:14 +0200

    data for Rules 1 and 3

---
+++ src/generate_rule_03.R
@@ -0,0 +1,10 @@
+
+set.seed(1)
+out <- data.frame(
+   level1 = sample(c("high","medium","low"), size=1000, replace=TRUE, prob=c(0.33,0.33,0.33))
+ , level2 = sample(c("high","medium","low"), size=1000, replace=TRUE, prob=c(0.05,0.9,0.05))
+ , level3 = sample(c("high","medium","low",NA), size=1000, replace=TRUE, prob=rep(0.25,4))
+)
+
+write.csv(out,file="Rule_03.csv")
+
