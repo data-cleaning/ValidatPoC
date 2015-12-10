73f8c8b49c2a4110392b4dc9415fbcd81138e33d
mark <mark.vanderloo@gmail.com>
2015-12-10 11:24:23 +0100

    some analyses

--- analysis.R
+++ analysis.R
@@ -2,6 +2,11 @@
 # 
 
 
+library(reshape2)
+library(RColorBrewer)
+library(whisker)
+library(stringr)
+
 ## Define some utility functions ----
 
 remove_comments <- function(x,lang=c("VTL","eStatistik","validate")){
@@ -36,15 +41,39 @@ code_count <- function(file,lang){
   c(lines=length(lines), characters=sum(nchar(lines)))
 }
 
+read_txt <- function(file){
+  txt <- readLines(file,warn=FALSE)
+  paste0(txt,"\n")
+}
+
+
+# return a latex one-pager with VTL, eStatistik and validate implementations
+texpage <- function(){
+"
+Rule {{nr}}: Natural language.
+\\begin{quote}
+{{NL}}
+\\end{quote}
+The VTL implementation.
+\\begin{verbatim}
+{{{VTL}}}
+\\end{verbatim}
+The eStatistik implementation.
+\\begin{verbatim}
+{{eStatistik}}
+\\end{verbatim}
+The validate implementation.
+\\begin{verbatim}
+{{validate}}
+\\end{verbatim}
+"
+}
 
 
 
 
-
-
-## ----
-
-# RESULTS 
+###############################################################################
+# Read in the implementation data and do character counts
 
 L <- list(
    eStatistik = dir("implementations/eStatistik/rules/",pattern = "Rule_[0-9]{2}.txt",full.names=TRUE)
@@ -52,6 +81,7 @@ L <- list(
   , validate = dir("implementations/validate/",pattern="rule_[0-9]{2}.txt",full.names=TRUE)
 )
 
+# count characters, lines
 M <- lapply(names(L), function(lang){
       files <- L[[lang]]
       A <- sapply(files, code_count , lang=lang)
@@ -60,6 +90,7 @@ M <- lapply(names(L), function(lang){
 })
 names(M) <- names(L)
 
+# gather results in a list of data.frames
 M <- lapply(names(M), function(lang){
   d <- as.data.frame(M[[lang]])
   d$lang <- lang
@@ -67,6 +98,7 @@ M <- lapply(names(M), function(lang){
   d
 })
 
+# order counts into a single result
 out <- do.call(rbind,M)
 out <- out[c(4,3,1,2)]
 out <- out[order(out$rule),]
@@ -75,35 +107,61 @@ out$lang <- factor(out$lang,levels=c("VTL","eStatistik","validate"))
 # write result to file.
 write.csv(out,file="code_counts.csv")
 
-library(ggplot2)
-qplot(data=out, x=rule,y=characters, geom="bar",stat="identity",facets=~lang)
+###############################################################################
+# Plot character counts
 
 
-library(reshape2)
 A <- reshape2::acast(data=out, formula = rule ~ lang, value.var="characters")
 
-library(RColorBrewer)
 pal <- brewer.pal(n=3,"Dark2")
 
-pdf(file="barplot.pdf",width=8.5,height=5.5)
-
+#pdf(file="barplot.pdf",width=8.5,height=5.5)
 barplot(t(prop.table(A[,c(3,1,2)],1)),horiz=TRUE,las=1
         ,col=pal,ylab="rule no.",cex.axis=1.2,cex.lab=1.2
         ,main="Relative nr of characters")
 par(xpd=TRUE)
 legend(x=0.3,y=24,legend=c("VTL","eStatistik","Validate"),fill=pal,horiz=TRUE,bty="n",cex=1.2)
+#dev.off()
 
-dev.off()
 
-plot(A[,1],type='step')
-lines(A[,2],col='red',type='step')
-lines(A[,3],col='green',type='step')
+#pdf(file="boxplot.pdf",width=8.5,height=5.5)
+boxplot(characters ~ lang, data=out,las=1
+        , main="Number of characters"
+        ,cex.main=1.2,cex.lab=1.2,cex.axis=1.2)
+#dev.off()
 
-pdf(file="boxplot.pdf",width=8.5,height=5.5)
-  boxplot(characters ~ lang, data=out,las=1
-          , main="Number of characters"
-          ,cex.main=1.2,cex.lab=1.2,cex.axis=1.2)
-dev.off()
+###############################################################################
+# Get nat. lang descriptions and create TeX onepagers.
+
+lines <- readLines("README.md")
+I <- grep("```",lines)
+lines[I[c(TRUE,FALSE)]] <- "\\begin{verbatim}"
+lines[I[c(FALSE,TRUE)]] <- "\\end{verbatim}"
+txt <- paste0(lines,collapse="\n")
+txt <- strsplit(txt,"#####",fixed=TRUE)[[1]][-1]
+rules <- str_extract(txt,".*?[0-9]{1,2}") 
+txt %<>% str_replace(".*?[0-9]{1,2}","") %>%     # remove rule number prefix
+  str_replace_all("_(.*?)_","\\\\emph{\\1}") %>% # emphasis
+  str_replace_all("_","\\\\_") %>%               # escape underscores
+  str_replace_all("`(.*?)`","\\\\code{\\1}")     # inline code
+
+tex <- vector(mode="character",length=18)
+for ( i in 1:18 ){
+  lng <-  list(
+    nr = rules[i], 
+    NL = txt[i]
+    , VTL = read_txt(L$VTL[i])
+    , eStatistik = read_txt(L$eStatistik[i])
+    , validate = read_txt(L$validate[i])
+  )
+  tex[i] <- whisker.render(texpage(),data=lng)
+  tex[i] <- gsub("\\n[[:blank:]]*?,","\n",tex[i])
+}
+
+texpages <- paste0(tex,collapse="\n\n\\newpage\n")
+
+write(texpages,file="test.tex")
+
 
 
 
