ef7724e34a4abd3975704172ee26145eff2e32c4
mark <mark.vanderloo@gmail.com>
2015-12-14 10:11:03 +0100

    added overview of implementations

--- analysis.R
+++ analysis.R
@@ -43,32 +43,31 @@ code_count <- function(file,lang){
 
 read_txt <- function(file){
   txt <- readLines(file,warn=FALSE)
-  paste0(txt,"\n")
+  paste0(str_wrap(txt,indent=2),collapse="\n")
 }
 
 
 # return a latex one-pager with VTL, eStatistik and validate implementations
 texpage <- function(){
 "
-Rule {{nr}}: Natural language.
+\\subsubsection*{ {{{nr}}}: Natural language}
 \\begin{quote}
-{{NL}}
+{{{NL}}}
 \\end{quote}
-The VTL implementation.
+\\subsubsection*{The VTL implementation}
 \\begin{verbatim}
 {{{VTL}}}
 \\end{verbatim}
-The eStatistik implementation.
+\\subsubsection*{The eStatistik implementation}
 \\begin{verbatim}
-{{eStatistik}}
+{{{eStatistik}}}
 \\end{verbatim}
-The validate implementation.
+\\subsubsection*{The validate implementation.}
 \\begin{verbatim}
-{{validate}}
+{{{validate}}}
 \\end{verbatim}
 "
 }
-
 
 
 
@@ -116,18 +115,18 @@ A <- reshape2::acast(data=out, formula = rule ~ lang, value.var="characters")
 pal <- brewer.pal(n=3,"Dark2")
 
 #pdf(file="barplot.pdf",width=8.5,height=5.5)
-barplot(t(prop.table(A[,c(3,1,2)],1)),horiz=TRUE,las=1
-        ,col=pal,ylab="rule no.",cex.axis=1.2,cex.lab=1.2
-        ,main="Relative nr of characters")
-par(xpd=TRUE)
-legend(x=0.3,y=24,legend=c("VTL","eStatistik","Validate"),fill=pal,horiz=TRUE,bty="n",cex=1.2)
+# barplot(t(prop.table(A[,c(3,1,2)],1)),horiz=TRUE,las=1
+#         ,col=pal,ylab="rule no.",cex.axis=1.2,cex.lab=1.2
+#         ,main="Relative nr of characters")
+# par(xpd=TRUE)
+# legend(x=0.3,y=24,legend=c("VTL","eStatistik","Validate"),fill=pal,horiz=TRUE,bty="n",cex=1.2)
 #dev.off()
 
 
 #pdf(file="boxplot.pdf",width=8.5,height=5.5)
-boxplot(characters ~ lang, data=out,las=1
-        , main="Number of characters"
-        ,cex.main=1.2,cex.lab=1.2,cex.axis=1.2)
+# boxplot(characters ~ lang, data=out,las=1
+#         , main="Number of characters"
+#         ,cex.main=1.2,cex.lab=1.2,cex.axis=1.2)
 #dev.off()
 
 ###############################################################################
@@ -135,15 +134,29 @@ boxplot(characters ~ lang, data=out,las=1
 
 lines <- readLines("README.md")
 I <- grep("```",lines)
-lines[I[c(TRUE,FALSE)]] <- "\\begin{verbatim}"
-lines[I[c(FALSE,TRUE)]] <- "\\end{verbatim}"
+
+beginverb <- I[c(TRUE,FALSE)]
+endverb <- I[c(FALSE,TRUE)]
+lines[beginverb] <- "\\begin{verbatim}"
+lines[endverb] <- "\\end{verbatim}"
+
+iverb <- unlist(lapply(seq_len(length(beginverb)),function(i) (beginverb[i]:endverb[i]) ))
+verb <- logical(length(lines))
+verb[iverb] <- TRUE
+
 txt <- paste0(lines,collapse="\n")
 txt <- strsplit(txt,"#####",fixed=TRUE)[[1]][-1]
 rules <- str_extract(txt,".*?[0-9]{1,2}") 
-txt %<>% str_replace(".*?[0-9]{1,2}","") %>%     # remove rule number prefix
-  str_replace_all("_(.*?)_","\\\\emph{\\1}") %>% # emphasis
-  str_replace_all("_","\\\\_") %>%               # escape underscores
-  str_replace_all("`(.*?)`","\\\\code{\\1}")     # inline code
+txt[!verb] %<>% str_replace(".*?[0-9]{1,2}","") %>%     # remove rule number prefix
+   str_replace_all("_(.*?)_","\\\\emph{\\1}") %>%       # emphasis
+   str_replace_all("_","\\\\_") %>%                     # escape underscores
+   str_replace_all("`(.*?)`","\\\\code{\\1}") %>%       # inline code
+   str_replace_all("%","\\\\%")                         # percent 
+  
+
+
+txt <- gsub("\\[(.*?)\\]","\\1 ",txt)
+
 
 tex <- vector(mode="character",length=18)
 for ( i in 1:18 ){
@@ -159,8 +172,7 @@ for ( i in 1:18 ){
 }
 
 texpages <- paste0(tex,collapse="\n\n\\newpage\n")
-
-write(texpages,file="test.tex")
+write(texpages,file="report/appendix.tex")
 
 
 
